5d410fe5f09f98a9b94af3548397bfad21568027
evamomanyi <evanyaks13@gmail.com>
2026-04-20 14:46:08 +0300

    Modified index.js

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Write your solution in this file!
+const burgers = ["Humburgers", "Cheeseburger"]
+let featureDrink = "strawberry milkshake"
+
+function addburgers () {
+    const newburger = "Flatburger";
+    burgers.push(newburger);
+if (true) {
+    const anotherNewBurger = "Maple Bacon Burger";
+    burgers.push(anotherNewBurger);
+}
+}
+function changeFeatureDrink() {
+    featureDrink = "The JavaShake"
+}
+console.log(burgers)
